df5e8532f41fb4d99aa217e1d7e20e206989405f
Aprajita Kashyap <kashyapaprajita2@gmail.com>
2026-08-14 19:04:59 +0530

    chore: remove static export configuration and path imports from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  outputFileTracingRoot: path.join(__dirname),
   images: {
     unoptimized: true,
   },
